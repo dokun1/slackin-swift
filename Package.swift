93dd16f16400f5e78dfdb5b532ac0437558761a6
David-Okun <david.okun@ibm.com>
2018-04-09 12:14:57 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "slackin-swift",
+    dependencies: [
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.2.0")),
+      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
+      .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "6.1.0"),
+      .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
+      .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
+      .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.8.4"))
+    ],
+    targets: [
+      .target(name: "slackin-swift", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
+      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics","Health", "KituraStencil"]),
+
+      .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
+    ]
+)
