c51a5221b814f2c284de4f33e07b9570ec3d4593
David Okun <david@okun.io>
2019-02-21 14:41:05 -0600

    updated logo

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "slackin-swift",
     dependencies: [
-      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.4.0")),
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.6.0")),
       .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "6.1.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
-      .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.10.0")),
-      .package(url: "https://github.com/IBM-Swift/Kitura-OpenAPI.git", .upToNextMinor(from: "1.0.3"))
+      .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMajor(from: "1.10.0")),
+      .package(url: "https://github.com/IBM-Swift/Kitura-OpenAPI.git", .upToNextMajor(from: "1.0.3")),
+      .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", .upToNextMajor(from: "2.0.0")),
     ],
     targets: [
       .target(name: "slackin-swift", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics","Health", "KituraStencil", "KituraOpenAPI"]),
+      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics","Health", "KituraStencil", "KituraOpenAPI", "SwiftyRequest"]),
 
       .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
     ]
